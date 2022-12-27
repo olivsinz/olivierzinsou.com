cddd2abf188381934566e965d6abfbdad2b1b712
Oliversinz <zinsouoliviers@gmail.com>
2022-12-27 19:56:28 +0100

    wip

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -4,10 +4,9 @@
   <div class="max-w-4xl mx-auto min-h-screen mt-20 sm:mt-40 px-6 md:px-10">
     <h1 class="text-4xl sm:text-6xl font-extrabold">Hi, I'm Olivier 👋</h1>
 
-    <div class="text-lg sm:text-2xl mt-12 sm:mt-20">
-      <p>I am a full-stack software developer.</p>
+    <div class="text-xl sm:text-2xl mt-12 sm:mt-20">
       <p>
-        You can follow me on
+        I am a full-stack software developer. You can follow me on
         <a href="" class="underline decoration-sky-500/30">Twitter</a> where I
         mostly tweet about my journey. You can also find me on
         <a href="" class="underline decoration-sky-500/30">LinkedIn</a> and
