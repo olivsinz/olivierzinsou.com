17601968b70813082d95959b4f4e607d39b8a068
Olivier ZINSOU <zinsouoliviers@gmail.com>
2024-04-20 21:36:17 +0100

    pattern on background

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,43 +1,51 @@
 <script setup></script>
 
 <template>
-  <div class="max-w-xl md:max-w-3xl mx-auto xmt-20 xsm:mt-40 px-6 md:px-10">
-    <h1 class="text-4xl sm:text-6xl font-extrabold">Hi, I'm Olivier 👋</h1>
+  <div>
+    <div
+      class="max-w-xl md:max-w-3xl mx-auto xmt-20 xsm:mt-40 px-6 md:px-10 relative z-10"
+    >
+      <h1 class="text-4xl sm:text-6xl font-extrabold">Hi, I'm Olivier 👋</h1>
 
-    <div class="text-xl sm:text-2xl mt-12 sm:mt-20">
-      <p>
-        I am a Full Stack Software Developer. Detail-oriented, I focus on using
-        my skills to break problems down and deliver solutions that scale to the
-        needs of the business.
-      </p>
-      <p>
-        Enthusiastic about learning new things, I'm looking to solve issues on a
-        much larger scale. You can follow me on
-        <a
-          href="https://twitter.com/zinsouoliviers"
-          target="_blank"
-          class="after:absolute after:content-[''] after:-z-[1] after:-inset-x-[0.1em] after:top-[66%] after:bottom-0 after:bg-secondary/50 after:transition-top after:duration-200 after:ease-custom hover:after:top-0 relative"
-        >
-          Twitter
-        </a>
-        where I mostly tweet about my journey. You can also find me on
-        <a
-          href="https://www.linkedin.com/in/olivierzinsou/"
-          target="_blank"
-          class="after:absolute after:content-[''] after:-z-[1] after:-inset-x-[0.1em] after:top-[66%] after:bottom-0 after:bg-secondary/50 after:transition-top after:duration-200 after:ease-custom hover:after:top-0 relative"
-        >
-          LinkedIn
-        </a>
-        and
-        <a
-          href="https://github.com/olivsinz/"
-          target="_blank"
-          class="after:absolute after:content-[''] after:-z-[1] after:-inset-x-[0.1em] after:top-[66%] after:bottom-0 after:bg-secondary/50 after:transition-top after:duration-200 after:ease-custom hover:after:top-0 relative"
-        >
-          GitHub
-        </a>
-        .
-      </p>
+      <div class="text-xl sm:text-2xl mt-12 sm:mt-20">
+        <p>
+          I am a Full Stack Software Developer. Detail-oriented, I focus on
+          using my skills to break problems down and deliver solutions that
+          scale to the needs of the business.
+        </p>
+        <p>
+          Enthusiastic about learning new things, I'm looking to solve issues on
+          a much larger scale. You can follow me on
+          <a
+            href="https://twitter.com/zinsouoliviers"
+            target="_blank"
+            class="after:absolute after:content-[''] after:-z-[1] after:-inset-x-[0.1em] after:top-[66%] after:bottom-0 after:bg-secondary/50 after:transition-top after:duration-200 after:ease-custom hover:after:top-0 relative"
+          >
+            Twitter
+          </a>
+          where I mostly tweet about my journey. You can also find me on
+          <a
+            href="https://www.linkedin.com/in/olivierzinsou/"
+            target="_blank"
+            class="after:absolute after:content-[''] after:-z-[1] after:-inset-x-[0.1em] after:top-[66%] after:bottom-0 after:bg-secondary/50 after:transition-top after:duration-200 after:ease-custom hover:after:top-0 relative"
+          >
+            LinkedIn
+          </a>
+          and
+          <a
+            href="https://github.com/olivsinz/"
+            target="_blank"
+            class="after:absolute after:content-[''] after:-z-[1] after:-inset-x-[0.1em] after:top-[66%] after:bottom-0 after:bg-secondary/50 after:transition-top after:duration-200 after:ease-custom hover:after:top-0 relative"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+
+    <div class="bg-pattern h-full absolute top-0 inset-x-0 z-[0]">
+      <div class="w-full h-full bg-gradient-to-t from-white"></div>
     </div>
   </div>
 </template>
